377bd402187a7565da1c3c7afc4f8f116f023da8
Sebastian Cerda <scerdalipanovich@gmail.com>
2022-03-28 21:27:42 -0300

    Add logo and NavBar

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div>
+               <img className="Logo-main" alt="logo" src='https://cdn-icons.flaticon.com/png/512/2098/premium/2098402.png?token=exp=1648511069~hmac=c90d1226a4142b5e72b673e482c90779' /> 
+            </div>
+        )
+    }
+}
+
+export default Logo;
